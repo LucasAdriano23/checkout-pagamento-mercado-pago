0bfc5bdd48a65f6965bf58df0eae0e630433b734
Lucas Adriano <lucasadrianobs@gmail.com>
2026-08-13 01:05:33 -0300

    Add enums

---
+++ app/Enums/OrderStatusEnum.php
@@ -0,0 +1,38 @@
+<?php 
+
+namespace App\Enums;
+
+enum OrderStatusEnum: int 
+{
+    case CART = 1;
+    CASE PENDING = 2;
+    CASE PAID = 3;
+    CASE CANCELED = 4;
+    CASE REJECT = 5;
+
+    public function getName()
+    {
+        return match($this) 
+        {
+            self::CART => 'Criado',
+            self::PENDING => 'Pendente',
+            self::PAID => 'Pago',
+            self::CANCELED => 'Cancelado',
+            self::REJECTED => 'Não aprovado',
+            default => 'Status não encontrado'
+        };
+    }
+
+    public function getStyles(): string
+    {
+        return match($this) 
+        {
+            self::CART => 'px-2 py-0.5 text-xs rounded-full bg-gray-100 text-gray-100',
+            self::PENDING => 'px-2 py-0.5 text-xs rounded-full bg-yellow-100 text-yellow-800',
+            self::PAID => 'px-2 py-1 text-rs rounded-full bg-green-100 text-green-800',
+            self::CANCELED => 'px-2 py-0.5 text-rs rounded-full bg-red-100 text-red-800',
+            self::REJECTED => 'Não aprovado',
+            default => ''
+        };
+    }
+}
