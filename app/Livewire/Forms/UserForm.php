c92b1522730fb4a838ba80f338f60cb5d8489124
Lucas Adriano <lucasadrianobs@gmail.com>
2026-08-14 16:58:41 -0300

    add userForm and AdressForm

---
+++ app/Livewire/Forms/UserForm.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use Livewire\Attributes\Rule;
+use Livewire\Form;
+
+class UserForm extends Form
+{   #[Rule('required|email|unique:users,email')]
+    public $email = "";
+
+    #[Rule('required|min:3|max:255')]
+    public $name = "";
+}
