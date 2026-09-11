e2bb751a89fdfaca8e2e43559d001aa8f9e67957
Lucas Adriano <lucasadrianobs@gmail.com>
2026-09-10 21:05:35 -0300

    fix:: order status enum reject name and styles

--- app/Enums/OrderStatusEnum.php
+++ app/Enums/OrderStatusEnum.php
@@ -18,20 +18,20 @@ enum OrderStatusEnum: int
             self::PENDING => 'Pendente',
             self::PAID => 'Pago',
             self::CANCELED => 'Cancelado',
-            self::REJECTED => 'Não aprovado',
+            self::REJECT => 'Não aprovado',
             default => 'Status não encontrado'
         };
     }
 
     public function getStyles(): string
     {
-        return match($this) 
+        return match($this)
         {
             self::CART => 'px-2 py-0.5 text-xs rounded-full bg-gray-100 text-gray-100',
             self::PENDING => 'px-2 py-0.5 text-xs rounded-full bg-yellow-100 text-yellow-800',
             self::PAID => 'px-2 py-1 text-rs rounded-full bg-green-100 text-green-800',
             self::CANCELED => 'px-2 py-0.5 text-rs rounded-full bg-red-100 text-red-800',
-            self::REJECTED => 'Não aprovado',
+            self::REJECT => 'px-2 py-0.5 text-xs rounded-full bg-red-100 text-red-800',
             default => ''
         };
     }
