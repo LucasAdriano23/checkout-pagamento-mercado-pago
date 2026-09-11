2a6ba4fafea97962e3364cea2d9449dc6cb3adea
Lucas Adriano <lucasadrianobs@gmail.com>
2026-09-10 22:20:58 -0300

    fix: add foreign key  order_id shippings

--- database/migrations/2026_08_12_224635_create_shippings_table.php
+++ database/migrations/2026_08_12_224635_create_shippings_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('shippings', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('order_id')->constrained();
             $table->string('address');
             $table->string('city');
             $table->string('state');
